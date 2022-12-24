815f017209e632c42483310a620dac44902c9c7a
tyonweekes <92998721+tyonweekes@users.noreply.github.com>
2022-12-24 09:40:50 -0500

    Fixed stretched error icon issue.
    Fixed stretched error icon issue.

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const formBtn = document.querySelector('.form-button');
-const emailInput = document.querySelector('#email');
+const input = document.getElementById("input");
 
-formBtn.addEventListener('click', e => {
-    if (emailInput.value < 1) {
-        document.querySelector('.container-form-input input').style.border = '1px solid hsl(0, 93%, 68%)';
-        document.querySelector('.img-error').style.display = 'flex';
-        document.querySelector('.error-message').style.display = 'flex';
-        document.querySelector('.error-message').innerHTML = 'The field cannot be empty.';
-    }
-    else {
-        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const submit = document.getElementById("button");
+const error = document.getElementById("error-text");
+const success = document.getElementById("success-text");
+const iconError = document.getElementById("icon-error");
 
-        if (regex.test(emailInput.value.toLowerCase()) == false) {
-            document.querySelector('.container-form-input input').style.border = '1px solid hsl(0, 93%, 68%)';
-            document.querySelector('.img-error').style.display = 'flex';
-            document.querySelector('.error-message').style.display = 'flex';
-            document.querySelector('.error-message').innerHTML = 'Invalid email format.';
+function Init() {
+    submit.addEventListener("click", function() {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (!re.test(input.value.toLowerCase())) {
+            error.style.display = "block";
+            iconError.style.display = "block";
+            success.style.display = "none";
+            input.style.borderColor = "red";
+        } else {
+            input.style.borderColor = "hsl(0, 36%, 70%)";
+            error.style.display = "none";
+            iconError.style.display = "none";
+            success.style.display = "block";
         }
-    }
-})
+        input.value = "";
+    });
+}
+
+Init();
+
+// const formBtn = document.querySelector('.form-button');
+// const emailInput = document.querySelector('#email');
+
+// formBtn.addEventListener('click', e => {
+//     if (emailInput.value < 1) {
+//         document.querySelector('.container-form-input input').style.border = '1px solid hsl(0, 93%, 68%)';
+//         document.querySelector('.img-error').style.display = 'flex';
+//         document.querySelector('.error-message').style.display = 'flex';
+//         document.querySelector('.error-message').innerHTML = 'The field cannot be empty.';
+//     }
+//     else {
+//         let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+//         if (regex.test(emailInput.value.toLowerCase()) == false) {
+//             document.querySelector('.container-form-input input').style.border = '1px solid hsl(0, 93%, 68%)';
+//             document.querySelector('.img-error').style.display = 'flex';
+//             document.querySelector('.error-message').style.display = 'flex';
+//             document.querySelector('.error-message').innerHTML = 'Invalid email format.';
+//         }
+//     }
+// })
