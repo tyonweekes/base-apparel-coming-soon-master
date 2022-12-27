3b7bede11e3217edeb11e8c275da3cc36269e917
tyonweekes <92998721+tyonweekes@users.noreply.github.com>
2022-12-26 22:42:18 -0500

    Cleaned up code. Completed challenge.
    Cleaned up code. Completed challenge.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const input = document.getElementById("input");
+const input = document.getElementById("email-input");
 
-const submit = document.getElementById("button");
+const submit = document.getElementById("submit-button");
 const error = document.getElementById("error-text");
 const success = document.getElementById("success-text");
 const iconError = document.getElementById("icon-error");
 
-function Init() {
+function customValidation() {
     submit.addEventListener("click", function() {
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -24,26 +24,4 @@ function Init() {
     });
 }
 
-Init();
-
-// const formBtn = document.querySelector('.form-button');
-// const emailInput = document.querySelector('#email');
-
-// formBtn.addEventListener('click', e => {
-//     if (emailInput.value < 1) {
-//         document.querySelector('.container-form-input input').style.border = '1px solid hsl(0, 93%, 68%)';
-//         document.querySelector('.img-error').style.display = 'flex';
-//         document.querySelector('.error-message').style.display = 'flex';
-//         document.querySelector('.error-message').innerHTML = 'The field cannot be empty.';
-//     }
-//     else {
-//         let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-//         if (regex.test(emailInput.value.toLowerCase()) == false) {
-//             document.querySelector('.container-form-input input').style.border = '1px solid hsl(0, 93%, 68%)';
-//             document.querySelector('.img-error').style.display = 'flex';
-//             document.querySelector('.error-message').style.display = 'flex';
-//             document.querySelector('.error-message').innerHTML = 'Invalid email format.';
-//         }
-//     }
-// })
+customValidation();
